af93f58a8ead82267aeb5964d54ce226b472b825
feinimouse <704602977@qq.com>
2019-04-06 15:54:47 +0800

    页导航栏开发

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,34 +1,50 @@
-import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Nav from "react-bootstrap/Nav";
-import Form from "react-bootstrap/Form";
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button'
+import React from 'react';
+import Menu from 'antd/lib/menu';
+import Input from 'antd/lib/input';
 import './Navbar.css';
 
-export default function() {
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="#home">Feinimouse</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-collapse" />
-            <Navbar.Collapse id="basic-navbar-collapse">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Form inline className="basic-navbar-form">
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+export default class Navbar extends React.Component {
+    state = {
+        page: 'guitar',
+    }
+
+    handleClick = (e) => {
+        this.setState({
+            page: e.key,
+        });
+        console.log('menu change to ', e.key);
+    };
+
+    handleSearch = (value) => {
+        console.log('search for ', value);
+    }
+
+    render() {
+        const { page } = this.state;
+        return (
+            <header className="nav flex-between">
+                <div className="ds-ib">
+                    <a href="./#">
+                        <h1 className="nav-title ds-ib">Feinimouse</h1>
+                    </a>
+                    <Input.Search
+                        placeholder="搜索"
+                        onSearch={this.handleSearch}
+                        className="nav-search"
+                    />
+                </div>
+                <nav className="ds-ib">
+                    <Menu
+                        onClick={this.handleClick}
+                        mode="horizontal"
+                        selectedKeys={[page]}
+                    >
+                        <Menu.Item key="guitar">吉他工具</Menu.Item>
+                        <Menu.Item key="blog">博客</Menu.Item>
+                        <Menu.Item key="coin">feinicoin</Menu.Item>
+                    </Menu>
+                </nav>
+            </header>
+        );
+    }
 }
